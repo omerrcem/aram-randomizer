a17983e40e853a416a2fae8ca965df6e61e46726
maro-omercem <omercem.turan@maroteknoloji.com>
2023-05-04 21:58:43 +0300

    min width

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <Head>
         <title>LoL Aram Randomizer</title>
         <meta name="description" content="Aram randomizer By Omer Cem Turan" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=800px, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NameEntry />
