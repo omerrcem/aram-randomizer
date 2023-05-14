f63f72414c4967ea4ab4caaa9ab22cadbaf9a0de
maro-omercem <omercem.turan@maroteknoloji.com>
2023-05-14 11:31:23 +0300

    Init analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '@/styles/globals.css';
 import '@/styles/custom.scss';
+import { Analytics } from '@vercel/analytics/react';
+
 
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Component {...pageProps} />
+			<Analytics />
+		</>
+	);
 }
