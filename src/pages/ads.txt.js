cbf1beea6f8eedb5b1524d6013f118163afeb530
Ömer Cem Turan <48556047+omerrcem@users.noreply.github.com>
2023-06-01 10:41:39 +0300

    Update ads.txt.js

--- src/pages/ads.txt.js
+++ src/pages/ads.txt.js
@@ -1,7 +1,7 @@
 function Ads() {
 };
 
-export async const getServerSideProps ({ res }) => {
+export async const getServerSideProps = ({ res }) => {
   	res.setHeader('Content-Type', 'text/plain');
     res.write('google.com, pub-7181315815609537, DIRECT, f08c47fec0942fa0');
     res.end();
